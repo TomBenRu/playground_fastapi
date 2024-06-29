dec3cd98589e230a4f829464eca58cbc206c8786
tombe <mail@thomas-ruff.de>
2024-06-29 04:01:48 +0200

    work in progress

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Form, HTTPException, Request
+from fastapi import FastAPI, Form, HTTPException, Request, Header
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -20,11 +20,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
+    print('in root', flush=True)
     return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/login/", response_class=HTMLResponse)
 async def login(request: Request, username: str = Form(...), password: str = Form(...)):
+    print('in login', flush=True)
     if username in users_db and users_db[username] == password:
         return RedirectResponse("/index/", status_code=302)
     else:
@@ -34,8 +36,11 @@ async def login(request: Request, username: str = Form(...), password: str = For
 
 
 @app.get("/login/", response_class=HTMLResponse)
-async def login(request: Request):
-    if request.headers.get("HX-Request"):
+async def login(request: Request, hx_request: str | None = Header(default=None)):
+    print('in login', flush=True)
+    print(request.headers.get("HX-Request"), flush=True)
+    if hx_request:  # alternative: request.headers.get("HX-Request"):
+        print('in hx-request', flush=True)
         return templates.TemplateResponse("login_form_with_prompt.html", {"request": request})
     return templates.TemplateResponse("login_form.html", {"request": request})
 
